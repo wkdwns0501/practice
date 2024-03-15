739d071a1ae435210ae7ebe6df20e499fd638515
wkdwns0501 <britain0501@naver.com>
2024-03-15 23:28:20 +0900

    게시판 연습 기본세팅

---
+++ boardPractice/src/main/java/com/board/demo/BoardPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.board.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardPracticeApplication.class, args);
+	}
+
+}
